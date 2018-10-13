dc52c590b8ce0875ad16f1a728781414c22a1c23
Ryan Luu <ryanluu12345@gmail.com>
2018-10-13 01:37:38 -0700

    First commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Image source = {{uri:'https://barkpost.com/wp-content/uploads/2015/02/featmeme.jpg'}} style={{width:400,height:400}} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
