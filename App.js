f513b38a0d59b2c4de0ce20ae24d20fa851c3845
Meghana Jain <meghanajain101@yahoo.com>
2018-10-13 02:12:01 -0700

    Added Landing Page

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import LandingPage from 'landing_page'
 import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
---
+++ landing_page.js
@@ -0,0 +1,13 @@
+import { Button } from 'react-native';
+
+class LandingPage extends Component {
+  render() {
+    return (
+    <Button
+      title="Continue ->"
+      color="#ADD8E6"
+    />
+    );
+
+  }
+}
